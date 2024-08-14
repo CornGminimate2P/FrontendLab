f71c61cbaf4b5a02f4fb495ccebee5fe82f0c500
CornGminimate2P <ftluconcrete@gmail.com>
2024-08-14 18:05:58 +0700

    IDK

---
+++ VUELab/vue-project/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Student from '../components/Student.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <Student name="KOSOG" lname="KOGRABAUE"></Student>
+    <TheWelcome />
+  </main>
+</template>
---
+++ VUELab/vue-project/src/views/MUT.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Student from '../components/Student.vue';
+</script>
+
+<template>
+    <div>
+        <h1>Hello MF!!</h1>
+        <Student name="Gay" lname="OHM"></Student>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        Student
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
